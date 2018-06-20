ee53eeffe4af6e92be1fc0f8af750a0128c3ae95
Eric Mill <eric.mill@gsa.gov>
2018-06-20 17:52:00 -0400

    accept just about any URL

--- assets/js/service-tracking.js
+++ assets/js/service-tracking.js
@@ -16,15 +16,25 @@ var Tracker = {
   },
 
   jsonFor: function(hostname) {
+    var location;
+    if (hostname.split(".").length == 2)
+      location = "parents";
+    else
+      location = "subdomains/scan";
+
     var scanner = "pshtt";
     var bucket = "cg-4adefb86-dadb-4ecf-be3e-f1c7b4f6d084";
     var region = "us-gov-west-1";
-    return "https://s3-" + region + ".amazonaws.com/" + bucket + "/live/subdomains/scan/cache/" + scanner + "/" + hostname + ".json";
+
+    return "https://s3-" + region + ".amazonaws.com/" + bucket + "/live/" + location + "/cache/" + scanner + "/" + hostname + ".json";
   },
 
-  // TODO: sanitize
   hostnameFor: function(input) {
-    return input;
+    var hostname = input;
+    hostname = hostname.replace(/^https?:\/\//, ''); // drop protocol
+    hostname = hostname.replace(/^www\./, ''); // drop www prefix
+    hostname = hostname.replace(/\/.*$/, ''); // query string and hash
+    return hostname;
   }
 };
 
