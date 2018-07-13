044ad71cdb1dd44aea4bec5e9330d5742e2f1ddf
Eric Mill <eric.mill@gsa.gov>
2018-07-13 13:07:33 -0400

    first pass at producing a report on where the USWDS is present

--- scanning/process.py
+++ scanning/process.py
@@ -22,14 +22,23 @@ def run(options):
 
   # collect list of dicts to convert into JSON
   bad_banner = []
+  uswds_present = []
 
   with open(uswds_csv, newline='') as csvfile:
     for dict_row in csv.DictReader(csvfile):
 
       has_bad_banner = utils.boolean_for(dict_row["USWDS Bad Banner Text"])
+      is_uswds_present = utils.boolean_for(dict_row["USWDS Present"])
 
       if (has_bad_banner):
         bad_banner.append({
+          'hostname': dict_row["Domain"],
+          'base_domain': dict_row["Base Domain"],
+          'scanned_url': dict_row["Scanned URL"]
+        })
+
+      if (is_uswds_present):
+        uswds_present.append({
           'hostname': dict_row["Domain"],
           'base_domain': dict_row["Base Domain"],
           'scanned_url': dict_row["Scanned URL"]
@@ -39,6 +48,10 @@ def run(options):
   bad_banner_data = utils.json_for(bad_banner)
   bad_banner_output = os.path.join(output_path, "bad_banner.json")
   utils.write(bad_banner_data, bad_banner_output)
+
+  uswds_present_data = utils.json_for(uswds_present)
+  uswds_present_output = os.path.join(output_path, "uswds_present.json")
+  utils.write(uswds_present_data, uswds_present_output)
 
 
 if __name__ == '__main__':
